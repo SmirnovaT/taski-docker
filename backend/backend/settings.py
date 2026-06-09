0db16136ace7d33becbb1612247eda67ef174912
tanya <tanya@example.com>
2026-06-09 23:56:11 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '130.49.172.90', 'taskitanya.serveftp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '130.49.172.90', 'taskitanya.serveftp.com', '0.0.0.0']
 
 
 INSTALLED_APPS = [
@@ -116,5 +116,6 @@ STATIC_ROOT = '/backend_static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://taskitanya.serveftp.com'
 ]
